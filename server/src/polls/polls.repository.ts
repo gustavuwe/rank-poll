4ab0f052c00f92c77f4c2cdb2c2a03dce2b64f3f
Gustav Uwe <gustavuwe.123@gmail.com>
2024-10-31 18:12:55 -0300

    feat(poll): add create and get features in repository

--- server/src/polls/polls.repository.ts
+++ server/src/polls/polls.repository.ts
@@ -1,7 +1,14 @@
-import { Inject, Injectable, Logger } from "@nestjs/common";
-import type { ConfigService } from "@nestjs/config";
-import type { Redis } from "ioredis";
-import { IORedisKey } from "src/redis.module";
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import type { ConfigService } from '@nestjs/config';
+import type { Redis } from 'ioredis';
+import { IORedisKey } from 'src/redis.module';
+import type { CreatePollData } from './types';
+import type { Poll } from '../../../shared';
 
 @Injectable()
 export class PollsRepository {
@@ -10,8 +17,71 @@ export class PollsRepository {
 
   constructor(
     configService: ConfigService,
-    @Inject(IORedisKey) private readonly redisClient: Redis
+    @Inject(IORedisKey) private readonly redisClient: Redis,
   ) {
     this.ttl = configService.get('POLL_DURATION');
+  }
+
+  async createPoll({
+    votesPerVoter,
+    topic,
+    pollID,
+    userID,
+  }: CreatePollData): Promise<Poll> {
+    const initialPoll = {
+      id: pollID,
+      topic,
+      votesPerVoter,
+      participants: {},
+      adminID: userID,
+    };
+
+    this.logger.log(
+      `Creating poll: ${JSON.stringify(initialPoll, null, 2)} with TTL ${
+        this.ttl
+      }`,
+    );
+
+    const key = `polls:${pollID}`;
+
+    try {
+      await this.redisClient
+        .multi([
+          ['send_command', 'JSON.SET', key, '.', JSON.stringify(initialPoll)],
+          ['expire', key, this.ttl],
+        ])
+        .exec();
+      return initialPoll;
+    } catch (error) {
+      this.logger.error(
+        `Failed to add poll ${JSON.stringify(initialPoll)}\n${error}`,
+      );
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async getPoll(pollID: string): Promise<Poll> {
+    this.logger.log(`Attempting to get poll with: ${pollID}`);
+
+    const key = `polls:${pollID}`;
+
+    try {
+      const currentPoll = await this.redisClient.send_command(
+        'JSON.GET',
+        key,
+        '.',
+      );
+
+      this.logger.verbose(currentPoll);
+
+      // if (currentPoll?.hasStarted) {
+      //   throw new BadRequestException('The poll has already started');
+      // }
+
+      return JSON.parse(currentPoll);
+    } catch (e) {
+      this.logger.error(`Failed to get pollID ${pollID}`);
+      throw new InternalServerErrorException(`Failed to get pollID ${pollID}`);
+    }
   }
 }
